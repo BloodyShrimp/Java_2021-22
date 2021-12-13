ab9928b6436e09a22eec35c847d17354f8261356
BloodyShrimp <szymkowerk@gmail.com>
2021-12-13 17:21:22 +0100

    Zadanie8 poprawione

--- Zadanie08/src/Shop.java
+++ Zadanie08/src/Shop.java
@@ -4,31 +4,40 @@ import java.util.Map;
 public class Shop implements ShopInterface {
     Map<String, Integer> current_stock = new HashMap<>();
     Map<String, Object> locks = new HashMap<>();
+    Object maps_lock = new Object();
 
     public void delivery(Map<String, Integer> goods) {
         for (Map.Entry<String, Integer> entry : goods.entrySet()) {
-            locks.putIfAbsent(entry.getKey(), new Object());
+            synchronized (maps_lock) {
+                locks.putIfAbsent(entry.getKey(), new Object());
+            }
             synchronized (locks.get(entry.getKey())) {
-                current_stock.merge(entry.getKey(), entry.getValue(), (a, b) -> a + b);
-                locks.get(entry.getKey()).notifyAll();
+                synchronized (maps_lock) {
+                    current_stock.merge(entry.getKey(), entry.getValue(), (a, b) -> a + b);
+                    locks.get(entry.getKey()).notifyAll();
+                }
             }
         }
     }
 
     public boolean purchase(String productName, int quantity) {
-        locks.putIfAbsent(productName, new Object());
-        synchronized(locks.get(productName)) {
-            if(!current_stock.containsKey(productName) || !(current_stock.get(productName) >= quantity)) {
+        synchronized (maps_lock) {
+            locks.putIfAbsent(productName, new Object());
+        }
+        synchronized (locks.get(productName)) {
+            if (!current_stock.containsKey(productName) || !(current_stock.get(productName) >= quantity)) {
                 try {
                     locks.get(productName).wait();
-                } catch(Exception e) {}
+                } catch (Exception e) {
+                }
             }
 
-            if(current_stock.containsKey(productName) && current_stock.get(productName) >= quantity) {
-                current_stock.put(productName, current_stock.get(productName) - quantity);
+            if (current_stock.containsKey(productName) && current_stock.get(productName) >= quantity) {
+                synchronized (maps_lock) {
+                    current_stock.put(productName, current_stock.get(productName) - quantity);
+                }
                 return true;
-            }
-            else {
+            } else {
                 return false;
             }
         }
